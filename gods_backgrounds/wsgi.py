55473261efe0f31113c801f5cab488863484f12a
gusakmachine <utorrentfilibusters@gmail.com>
2021-05-11 17:10:56 +0300

    Rename to 'gods-backgrounds'

--- gods_backgrounds/wsgi.py
+++ gods_backgrounds/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for goodfon project.
+WSGI config for gods_backgrounds project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goodfon.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gods_backgrounds.settings')
 
 application = get_wsgi_application()
